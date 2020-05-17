db3d247a917d6d7653504a45eba021ed367c4f40
Leonardo Barcelos <leobarcelos12@gmail.com>
2020-05-17 20:05:11 -0300

    Configuração Heroku

--- sistemada/settings.py
+++ sistemada/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -157,4 +158,9 @@ COMPRESS_PRECOMPILERS = (
 # ID dos grupos padrão do DA
 GESTAO_DEFAULT_GROUP_LIST = [1, 2, 3, 4]
 
-# COMPRESS_ENABLED = True
+COMPRESS_ENABLED = True
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+django_heroku.settings(locals())
